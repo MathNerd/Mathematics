93f60d4f751238a85586f248b5392966455c7f31
MathNerd <Hhh.atias@gmail.com>
2018-08-09 15:49:36 +0300

    Create recursive_seqeunce_non_optimized.c

---
+++ InfinitesimalCalculus2/Technion/Lecture13/recursive_seqeunce_non_optimized.c
@@ -0,0 +1,27 @@
+#include<iostream>
+
+double a(int n)
+{
+    if (n==1)
+        return 0.25;
+    else if (n>1)
+    {
+        double a_prev = a(n-1);
+        return a_prev*a_prev + 0.25;
+    }
+    else
+    {
+        printf("FATAL ERROR!\n");
+        return 0.0;
+    }
+}
+
+int main()
+{
+    std::cout.precision(17);
+    
+    for (int i = 100; i<=100; i++)
+        std::cout << "a(" << i << ") = " << a(i) << std::endl;
+       
+    return 0; 
+}
