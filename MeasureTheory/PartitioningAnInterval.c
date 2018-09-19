557f3dfd035e7593424bb2d6997748a3c4d16636
MathNerd <Hhh.atias@gmail.com>
2018-09-19 09:34:45 +0300

    Update PartitioningAnInterval.c

--- MeasureTheory/PartitioningAnInterval.c
+++ MeasureTheory/PartitioningAnInterval.c
@@ -25,7 +25,20 @@ double partition_point(double a, double b, unsigned n, unsigned i)
 
 int main(void)
 {
-    partition_point(2.0,1.0,)
+    // a >= b
+    printf("%f\n", partition_point(2.0,1.0,0,4));
+    
+    // n < 1
+    printf("%f\n", partition_point(1.0,2.0,0,4));
+    
+    // i > n
+    printf("%f\n", partition_point(1.0,2.0,3,4));
+    
+    // good
+    printf("%f\n", partition_point(1.0,2.0,3,0));
+    printf("%f\n", partition_point(1.0,2.0,3,1));
+    printf("%f\n", partition_point(1.0,2.0,3,2)); 
+    printf("%f\n", partition_point(1.0,2.0,3,3));
     
     return 0;
 }
