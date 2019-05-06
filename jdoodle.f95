e815bd7e5339e5d6f9b056a423ccd0bf74c258fc
MathNerd <Hhh.atias@gmail.com>
2019-05-06 12:52:22 +0300

    Add files via upload

---
+++ jdoodle.f95
@@ -0,0 +1,19 @@
+﻿program sum 
+
+    do n = 1,30
+        PRINT *,h(n)
+    end do
+    
+contains
+
+function h(n) result(r)
+    integer, intent(in) :: n
+    integer :: r
+    if (modulo(n,2)==1) then
+        r = (1-n)/2
+    else !modulo(n,2)=0
+        r = n/2
+    end if 
+end function
+
+end program sum
