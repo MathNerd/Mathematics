125e15f0ee3d9be1a2d75b6c20c30df80f0062fa
MathNerd <Hhh.atias@gmail.com>
2019-05-06 13:12:37 +0300

    Update MappingNintoZ.cpp

--- MappingNintoZ.cpp
+++ MappingNintoZ.cpp
@@ -1,1 +1,20 @@
+// program to map N (the set of natural numbers) into Z (the set of whole numbers):
 
+#include <cstdio>
+#define modulo(N,P) ((N)%(P))
+
+int h(int n)
+{
+    int r;
+    if (modulo(n,2)==1)
+        r = (1-n)/2;
+    else
+        r = n/2;
+    return r;
+}
+
+int main(void)
+{
+    for(int n=1; n<=30; n++)
+        printf("\t%d\n",h(n));
+}
