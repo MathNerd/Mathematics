a7dcfeb37786f4a0d1cd60293b66a1cd33cf181d
MathNerd <Hhh.atias@gmail.com>
2019-05-06 13:03:56 +0300

    Create MappingNintoZ.cpp

---
+++ MappingNintoZ.cpp
@@ -0,0 +1,19 @@
+// program to map N (the set of natural numbers) into Z (the set of whole numbers):
+
+#include <cstdio>
+
+int h(int n)
+{
+    int r;
+    if (modulo(n,2)==1)
+        r = (1-n)/2;
+    else
+        r = n/2;
+    return r;
+}
+
+int main(void)
+{
+    for(int n=1; n<=30; n++)
+        printf("%d\n",h(n));
+}
