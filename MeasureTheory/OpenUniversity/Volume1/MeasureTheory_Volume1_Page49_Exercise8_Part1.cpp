d4d7fd9160bfb97089ee3ff4c3c5324c89bf460e
MathNerd <Hhh.atias@gmail.com>
2019-06-27 19:00:45 +0300

    Create MeasureTheory_Volume1_Page49_Exercise8_Part1.cpp

---
+++ MeasureTheory/OpenUniversity/Volume1/MeasureTheory_Volume1_Page49_Exercise8_Part1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+template<class T>
+T Max(T x, T y)
+{
+    return (x < y ? y : x);
+}
+
+template<class T>
+T Max(T* a, size_t n)
+{
+    if (n == 1)
+        return a[0];
+    else /* n > 1 */
+        return Max(Max(a, n - 1), a[n - 1]);
+}
+
+int main() {
+	double a[] = {1,4,2,6,3,9,6,2,5,7,1,3};
+	const size_t n = sizeof(a)/sizeof(a[0]);
+	cout << "max(a," << n << ") = " << Max(a,n) << endl;
+	
+	return 0;
+}
